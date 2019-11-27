e5c8f7598112cef95c0adc4128d167dc2b8c6ec4
divyanshu1610 <divyanshu1610@gmail.com>
2019-11-27 12:06:33 +0530

    added mycp.c

--- mycp.c
+++ mycp.c
@@ -2,17 +2,45 @@
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
-int main(int argc,char *argv[])
-{
-char str[512]; int sz;
-int fd = open(argv[1], O_CREATE | O_RDONLY);
 
-printf(1, "%s\n", argv[2]);
-int fd1 = open(argv[2], O_CREATE | O_WRONLY);
-while((sz=read(fd, str,512)) > 0)
-write(fd1,str,sz);
-printf(1,"%d \t %d\n",fd1, fd);
-close(fd);
-close(fd1);
-exit();
+char buf[512];
+
+int main(int argc,char* argv[])
+{
+	int fd1,fd2;
+	if(argc!=3)
+	{
+		printf(1, "Usage: mycp arg1 arg2");
+		exit();
+	}
+	if((fd1=open(argv[1],O_RDONLY))<0){
+		printf(1, "mycp: cannot open %s\n", argv[1]);
+      exit();
+	}
+	
+	if((fd2=open(argv[2],O_CREATE|O_WRONLY))<0)	
+	{
+		printf(1, "mycp: cannot create %s\n", argv[2]);
+      exit();
+	}
+
+	int n;
+	while((n = read(fd1, buf, sizeof(buf))) > 0)
+	{
+		if (write(fd2, buf, n) != n) {
+      printf(1, "mycp: write error\n");
+      exit();
+    }
+  
+	  if(n < 0){
+	    printf(1, "mycp: read error\n");
+	    exit();
+	  }
+}
+
+
+	close(fd1);
+	close(fd2);
+	exit();
+
 }
